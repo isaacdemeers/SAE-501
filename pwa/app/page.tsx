d8e7c164b0b4a2c7503efa5e22fab1cb6f75bc74
Poldus <leopold.canou@gmail.com>
2024-10-15 16:05:44 +0200

    Migrate from pages to app

--- pwa/app/page.tsx
+++ pwa/app/page.tsx
@@ -17,7 +17,7 @@ const Welcome = () => (
     <Head>
       <title>Welcome to API Platform!</title>
     </Head>
-    <section className="w-full bg-spider-cover relative">
+    <section className="relative w-full bg-spider-cover">
       <a
         href="https://les-tilleuls.coop/en"
         target="_blank"
@@ -29,12 +29,12 @@ const Welcome = () => (
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
-            className="mx-2 w-5 h-5 fill-red-500"
+            className="w-5 h-5 mx-2 fill-red-500"
           >
             <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
           </svg>
           by
-          <div className="pl-2 flex items-center">
+          <div className="flex items-center pl-2">
             <Image
               alt="Les-Tilleuls.coop"
               src={logoTilleuls}
@@ -52,15 +52,15 @@ const Welcome = () => (
         </div>
         <div className="flex flex-1 flex-col items-center text-center | md:text-left md:items-start">
           <h1>
-            <span className="block text-4xl text-cyan-200 font-bold mb-2">
+            <span className="block mb-2 text-4xl font-bold text-cyan-200">
               Welcome to
             </span>
             <Image alt="API Platform" src={logo} />
           </h1>
-          <p className="text-cyan-200 my-5 text-lg">
+          <p className="my-5 text-lg text-cyan-200">
             This container will host your{" "}
             <a
-              className="text-white font-bold hover:bg-cyan-500"
+              className="font-bold text-white hover:bg-cyan-500"
               href="https://nextjs.org/"
             >
               <b>Next.js</b>
@@ -71,10 +71,10 @@ const Welcome = () => (
             target="_blank"
             rel="noopener noreferrer"
             href="https://api-platform.com/docs/"
-            className="bg-white text-cyan-700 px-8 py-3 relative overflow-hidden transition-all font-extrabold text-lg group hover:pl-4 hover:pr-12"
+            className="relative px-8 py-3 overflow-hidden text-lg font-extrabold transition-all bg-white text-cyan-700 group hover:pl-4 hover:pr-12"
           >
             Get started
-            <div className="absolute left-full top-0 w-7 h-full bg-cyan-200 transition-all flex p-1 justify-center items-center group-hover:-translate-x-full">
+            <div className="absolute top-0 flex items-center justify-center h-full p-1 transition-all left-full w-7 bg-cyan-200 group-hover:-translate-x-full">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
@@ -92,10 +92,10 @@ const Welcome = () => (
         </div>
       </div>
     </section>
-    <section className="bg-white py-8">
+    <section className="py-8 bg-white">
       <div className="container | md:px-20">
         <div className="text-center | lg:text-left lg:w-3/5 lg:ml-auto">
-          <h2 className="text-black text-md font-bold mb-5">
+          <h2 className="mb-5 font-bold text-black text-md">
             Available services:
           </h2>
           <div className="flex justify-center flex-wrap | lg:justify-start lg:grid lg:gap-5 lg:grid-cols-2">
@@ -118,11 +118,7 @@ const Welcome = () => (
         url="https://twitter.com/ApiPlatform"
         title="API Platform on Twitter"
       >
-        <svg
-          viewBox="0 0 20 20"
-          aria-hidden="true"
-          fill="currentColor"
-        >
+        <svg viewBox="0 0 20 20" aria-hidden="true" fill="currentColor">
           <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0 0 20 3.92a8.19 8.19 0 0 1-2.357.646 4.118 4.118 0 0 0 1.804-2.27 8.224 8.224 0 0 1-2.605.996 4.107 4.107 0 0 0-6.993 3.743 11.65 11.65 0 0 1-8.457-4.287 4.106 4.106 0 0 0 1.27 5.477A4.073 4.073 0 0 1 .8 7.713v.052a4.105 4.105 0 0 0 3.292 4.022 4.095 4.095 0 0 1-1.853.07 4.108 4.108 0 0 0 3.834 2.85A8.233 8.233 0 0 1 0 16.407a11.615 11.615 0 0 0 6.29 1.84"></path>
         </svg>
       </HelpButton>
@@ -174,15 +170,15 @@ const Card = ({
   title: string;
 }) => (
   <div className="w-full max-w-xs p-2 | sm:w-1/2 | lg:w-full lg:p-0">
-  <a
-    href={url}
-    className="w-full flex items-center flex-col justify-center shadow-card p-3 min-h-24 transition-colors text-cyan-500 border-4 border-transparent hover:border-cyan-200 hover:text-cyan-700 | sm:flex-row sm:justify-start sm:px-5"
-  >
-    <Image src={image} width="50" height="50" alt="" />
-    <h3 className="text-center text-base uppercase font-semibold leading-tight pt-3 | sm:text-left sm:pt-0 sm:pl-5">
-      {title}
-    </h3>
-  </a>
+    <a
+      href={url}
+      className="w-full flex items-center flex-col justify-center shadow-card p-3 min-h-24 transition-colors text-cyan-500 border-4 border-transparent hover:border-cyan-200 hover:text-cyan-700 | sm:flex-row sm:justify-start sm:px-5"
+    >
+      <Image src={image} width="50" height="50" alt="" />
+      <h3 className="text-center text-base uppercase font-semibold leading-tight pt-3 | sm:text-left sm:pt-0 sm:pl-5">
+        {title}
+      </h3>
+    </a>
   </div>
 );
 
@@ -195,14 +191,13 @@ const HelpButton = ({
   title: string;
   children: React.ReactNode;
 }) => (
-  (<Link
+  <Link
     href={url}
     target="_blank"
     rel="noopener noreferrer"
     className="w-12 h-12 p-2.5 rounded-full border-2 border-gray-100 justify-center transition-colors hover:border-cyan-200 hover:bg-cyan-200/50 m-2 inline-flex items-center | md:p-1 md:w-9 md:h-9 md:flex md:mx-auto md:m-0"
-    title={title}>
-
+    title={title}
+  >
     {children}
-
-  </Link>)
+  </Link>
 );
