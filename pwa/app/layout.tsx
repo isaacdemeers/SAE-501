f71b2acfeb851ee32d43d9809b0ae73d80158a7b
Léopold Canou <52009517+leopoldcanou@users.noreply.github.com>
2024-11-13 15:32:50 +0100

    Event Page

--- pwa/app/layout.tsx
+++ pwa/app/layout.tsx
@@ -1,12 +1,15 @@
 'use client'
 import "../styles/globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
