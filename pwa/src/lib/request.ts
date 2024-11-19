e3019ed6c13598c8c7c9ecab0175989332eb4c3e
Léopold Canou <52009517+leopoldcanou@users.noreply.github.com>
2024-11-19 17:44:36 +0100

    Fix data fetching in PageEvent

--- pwa/src/lib/request.ts
+++ pwa/src/lib/request.ts
@@ -22,8 +22,8 @@ export async function AddUser(data: any) {
 
 export async function VerifyEmailToken(data: string) {
   let formData = {
-    emailtoken: data
-  }
+    emailtoken: data,
+  };
   try {
     console.log(data);
     const response = await fetch(`${API_BASE_URL}/verify-email`, {
@@ -59,8 +59,8 @@ export async function LoginUser(data: any) {
 
 export async function TestEmail(data: string) {
   let formData = {
-    email: data
-  }
+    email: data,
+  };
   try {
     console.log(formData);
     const response = await fetch(`${API_BASE_URL}/users/testemail`, {
@@ -79,8 +79,8 @@ export async function TestEmail(data: string) {
 
 export async function TestUsername(data: string) {
   let formData = {
-    username: data
-  }
+    username: data,
+  };
   try {
     console.log(formData);
     const response = await fetch(`${API_BASE_URL}/users/testusername`, {
@@ -99,12 +99,16 @@ export async function TestUsername(data: string) {
 
 export async function GetGrettings() {
   try {
-    const jwtToken = document.cookie.split('; ').find(row => row.startsWith('jwt_token='))?.split('=')[1] || '';
+    const jwtToken =
+      document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("jwt_token="))
+        ?.split("=")[1] || "";
     const response = await fetch(`${API_BASE_URL}/greetings`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": jwtToken
+        Authorization: jwtToken,
       },
     });
     return await response.json();
@@ -142,6 +146,21 @@ export async function Newpass(data: any) {
     return await response.json();
   } catch (error) {
     console.error("Error adding user:", error);
+    throw error;
+  }
+}
+
+export async function GetEvent(id: number) {
+  try {
+    const response = await fetch(`${API_BASE_URL}/events/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return await response.json();
+  } catch (error) {
+    console.error("Error getting event:", error);
     throw error;
   }
 }
