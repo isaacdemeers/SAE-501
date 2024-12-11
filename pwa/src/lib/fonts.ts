226cbbcd33ac290209c09b3287d061c9dd77878b
Léopold Canou <52009517+leopoldcanou@users.noreply.github.com>
2024-12-11 12:15:26 +0100

    fix fonts directory

--- pwa/src/lib/fonts.ts
+++ pwa/src/lib/fonts.ts
@@ -3,11 +3,11 @@ import localFont from "next/font/local";
 export const inter = localFont({
   src: [
     {
-      path: "./fonts/Inter.woff2",
+      path: "../fonts/Inter.woff2",
       style: "normal",
     },
     {
-      path: "./fonts/Inter-Italic.woff2",
+      path: "../fonts/Inter-Italic.woff2",
       style: "italic",
     },
   ],
@@ -16,27 +16,27 @@ export const inter = localFont({
 export const abhayalibre = localFont({
   src: [
     {
-      path: "./fonts/AbhayaLibre-Regular.woff2",
+      path: "../fonts/AbhayaLibre-Regular.woff2",
       style: "normal",
       weight: "400",
     },
     {
-      path: "./fonts/AbhayaLibre-Medium.woff2",
+      path: "../fonts/AbhayaLibre-Medium.woff2",
       style: "normal",
       weight: "500",
     },
     {
-      path: "./fonts/AbhayaLibre-SemiBold.woff2",
+      path: "../fonts/AbhayaLibre-SemiBold.woff2",
       style: "normal",
       weight: "600",
     },
     {
-      path: "./fonts/AbhayaLibre-Bold.woff2",
+      path: "../fonts/AbhayaLibre-Bold.woff2",
       style: "normal",
       weight: "700",
     },
     {
-      path: "./fonts/AbhayaLibre-ExtraBold.woff2",
+      path: "../fonts/AbhayaLibre-ExtraBold.woff2",
       style: "normal",
       weight: "800",
     },
