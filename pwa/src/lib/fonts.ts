8f85b7f234f6bff0021effa6d05da433310fb4d4
Léopold Canou <52009517+leopoldcanou@users.noreply.github.com>
2024-12-11 11:27:44 +0100

    add local fonts

--- pwa/src/lib/fonts.ts
+++ pwa/src/lib/fonts.ts
@@ -1,11 +1,44 @@
-import { Abhaya_Libre, Inter } from 'next/font/google'
+import localFont from "next/font/local";
 
-export const abhayalibre = Abhaya_Libre({
-    subsets: ['latin'],
-    weight: ['400', '500', '600', '700']
-})
+export const inter = localFont({
+  src: [
+    {
+      path: "./fonts/Inter.woff2",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Inter-Italic.woff2",
+      style: "italic",
+    },
+  ],
+});
 
-export const inter = Inter({
-    subsets: ['latin'],
-    weight: ['400', '500', '600', '700']
-}) 
+export const abhayalibre = localFont({
+  src: [
+    {
+      path: "./fonts/AbhayaLibre-Regular.woff2",
+      style: "normal",
+      weight: "400",
+    },
+    {
+      path: "./fonts/AbhayaLibre-Medium.woff2",
+      style: "normal",
+      weight: "500",
+    },
+    {
+      path: "./fonts/AbhayaLibre-SemiBold.woff2",
+      style: "normal",
+      weight: "600",
+    },
+    {
+      path: "./fonts/AbhayaLibre-Bold.woff2",
+      style: "normal",
+      weight: "700",
+    },
+    {
+      path: "./fonts/AbhayaLibre-ExtraBold.woff2",
+      style: "normal",
+      weight: "800",
+    },
+  ],
+});
