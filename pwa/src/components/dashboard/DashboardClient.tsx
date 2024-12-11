aa4a4d3f7ed9d931f69d170e77c9ecdb48bc86a2
Fredeau123 <frederictrivett@gmail.com>
2024-12-11 09:11:20 +0100

    update working urls

--- pwa/src/components/dashboard/DashboardClient.tsx
+++ pwa/src/components/dashboard/DashboardClient.tsx
@@ -16,7 +16,7 @@ interface AuthResponse {
 }
 
 interface Event {
-    eventId: string;
+    id: string;
     title: string;
     description: string;
     datestart: string;
@@ -54,9 +54,10 @@ export default function DashboardClient() {
             if (!response.ok) throw new Error('Failed to fetch events');
 
             const { events, totalPages } = await response.json();
-            setTotalPages(totalPages);
+            console.log('API Response:', events);
+
             const formattedEvents = events.map((event: Event) => ({
-                id: event.eventId,
+                id: event.id,
                 imageUrl: event.img,
                 location: event.location,
                 title: event.title,
@@ -70,6 +71,7 @@ export default function DashboardClient() {
                 }),
                 buttonText: "Voir l'Événement"
             }));
+            console.log('Formatted Events:', formattedEvents);
             setSuggestedEvents(formattedEvents);
         } catch (error) {
             console.error('Error fetching upcoming events:', error);
